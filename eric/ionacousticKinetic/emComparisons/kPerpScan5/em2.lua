bf85978e37c5836e1b7b13166f65c672ad0ab58b
Eric Shi <eshi@pppl.gov>
2013-12-18 00:15:42 -0500

    Changes

--- eric/ionacousticKinetic/emComparisons/kPerpScan5/em2.lua
+++ eric/ionacousticKinetic/emComparisons/kPerpScan5/em2.lua
@@ -62,7 +62,7 @@ PL_ION, PU_ION = -6.0*ionMass*vtIon, 6.0*ionMass*vtIon
 
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 3e-5
+tEnd = 6e-5
 nFrames = 1
 
 -- A generic function to run an updater.
