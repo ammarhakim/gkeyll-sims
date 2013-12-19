589ade062d454723bb943d43203096bd7a902ad0
Eric Shi <eshi@pppl.gov>
2013-12-19 14:46:03 -0500

    Stuff

--- eric/ionacousticKinetic/emComparisons/beta_0_01_scans_2/position/64.sh
+++ eric/ionacousticKinetic/emComparisons/beta_0_01_scans_2/position/64.sh
@@ -2,11 +2,11 @@
 
 #PBS -N gke-kinetic-test
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
-#PBS -m aeb 
+#PBS -m ae 
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=1:ppn=1 
 #PBS -l mem=4gb 
-#PBS -l walltime=8:00:00
+#PBS -l walltime=16:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
