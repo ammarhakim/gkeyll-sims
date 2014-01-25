3d044ab3391e9cbf8f14275c9015c91c20fd9829
Eric Shi <eshi@pppl.gov>
2014-01-25 02:15:23 -0500

    New kPerp value to do tests at

--- eric/ionacousticKinetic/emComparisons/beta_0_01_scans/kPerp/computeKAWDispersion_scans.py
+++ eric/ionacousticKinetic/emComparisons/beta_0_01_scans/kPerp/computeKAWDispersion_scans.py
@@ -41,9 +41,9 @@ for index, kPerpRho in enumerate(kPerpRhoList):
   z0 = optimize.newton(eps,z0,derivEps,(beta_e,),tol,10000)
   exactFreqList[index] = fabs(z0.real*sqrt(2*Te0*eV/m_e)/vA);
 
-kPerpSimList = [0.01, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-tSimList = [2.8034e-6, 2.863e-6, 2.659e-6, 2.685e-6, 2.639e-6, 2.563e-6, 2.474e-6, 2.382e-6, 2.292e-6,
-    2.197e-6, 2.108e-6]
+kPerpSimList = [0.01, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+tSimList = [2.802e-6, 2.867e-6, 2.861e-6, 2.829e-6, 2.759e-6, 2.681e-6, 2.589e-6, 2.492e-6, 2.392e-6, 2.294e-6,
+    2.199e-6, 2.106e-6]
 omegaSimList = zeros(len(kPerpSimList))
 
 for index, kPerpRho in enumerate(kPerpSimList):
