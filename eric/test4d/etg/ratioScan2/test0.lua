3a9119704bd84308f042f069b86dcd8c4ebdda36
Eric Shi <eshi@pppl.gov>
2014-10-21 19:10:33 -0400

    Stuff

--- eric/test4d/etg/ratioScan2/test0.lua
+++ eric/test4d/etg/ratioScan2/test0.lua
@@ -8,7 +8,7 @@ polyOrder = 1
 cfl = 0.1
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 2e-6
+tEnd = 1.5e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
 nFrames = 2
 tFrame = (tEnd-tStart)/nFrames -- time between frames
