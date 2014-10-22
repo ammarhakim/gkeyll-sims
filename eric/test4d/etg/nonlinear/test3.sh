61a23c5d46e211d3203c76bf3a4a54a20937262f
Eric Shi <eshi@pppl.gov>
2014-10-22 18:00:23 -0400

    Stuff

---
+++ eric/test4d/etg/nonlinear/test3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#PBS -N gke-test3-nonlinear
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=1:ppn=1 
+#PBS -l mem=4gb 
+#PBS -l walltime=3:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+ 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i test3.lua -pc_type lu" 
+cd $PBS_O_WORKDIR 
+$CMD 
+exit
