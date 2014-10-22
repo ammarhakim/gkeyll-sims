c6f23c494b2417e1922c1a2860d516eee356df82
Eric Shi <eshi@pppl.gov>
2014-10-22 16:57:27 -0400

    Stuff

--- eric/test4d/etg/nonlinear/test2.sh
+++ eric/test4d/etg/nonlinear/test2.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
-#PBS -N gke-kinetic-test1-nonlinear
+#PBS -N gke-test2-nonlinear
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=1:ppn=1 
 #PBS -l mem=4gb 
-#PBS -l walltime=12:00:00
+#PBS -l walltime=24:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
  
-CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i test1.lua -pc_type lu" 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i test2.lua -pc_type lu" 
 cd $PBS_O_WORKDIR 
 $CMD 
 exit
