245a1874be8e1f5f37437ba8daf5ce6a7967d3e0
Eric Shi <eshi@pppl.gov>
2014-10-22 15:35:34 -0400

    Stuff

--- eric/test4d/etg/nonlinear/test0.lua
+++ eric/test4d/etg/nonlinear/test0.lua
@@ -8,9 +8,9 @@ polyOrder = 1
 cfl = 0.1
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 3e-6
+tEnd = 4e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
-nFrames = 3
+nFrames = 4
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 
 -- physical parameters
@@ -35,9 +35,9 @@ deltaR    = 32*rho_s
 L_T       = R/10
 ky_min    = 2*math.pi/deltaR
 -- grid parameters: number of cells
-N_X = 4
+N_X = 8
 N_Y = 8
-N_VPARA = 8
+N_VPARA = 4
 N_MU = N_VPARA/2
 -- grid parameters: domain extent
 X_LOWER = R
