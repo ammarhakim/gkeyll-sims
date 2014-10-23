b98ed7a81300f0a455edc2a62f409ba831a385e8
Eric Shi <eshi@pppl.gov>
2014-10-23 19:00:03 -0400

    Stuff

--- eric/test4d/etg/nonlinear/kzfScan/test2.lua
+++ eric/test4d/etg/nonlinear/kzfScan/test2.lua
@@ -9,9 +9,9 @@ polyOrder = 1
 cfl = 0.05
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 4e-6
+tEnd = 8e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
-nFrames = 16
+nFrames = 20
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 
 -- physical parameters
