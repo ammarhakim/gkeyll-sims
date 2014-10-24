a32f4c88596ffd5a28a88ba5ff07a58250af05dd
Eric Shi <eshi@pppl.gov>
2014-10-24 02:57:41 -0400

    Stuff

---
+++ eric/test4d/etg/nonlinear/kzfScan2/test1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#PBS -N kzf-1
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=1:ppn=1 
+#PBS -l mem=4gb 
+#PBS -l walltime=24:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+ 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i test1.lua -pc_type lu" 
+cd $PBS_O_WORKDIR 
+$CMD 
+exit
