4dee8ec43590e3bc0f1b4b68e71c55a0ecc9c972
Eric Shi <eshi@pppl.gov>
2014-10-25 19:09:10 -0400

    Stuff

--- eric/test4d/etg/ratioScan5/test2.lua
+++ eric/test4d/etg/ratioScan5/test2.lua
@@ -32,7 +32,7 @@ c_s       = math.sqrt(kineticTemp*eV/kineticMass)
 omega_s   = math.abs(kineticCharge*B0/kineticMass)
 rho_s     = c_s/omega_s
 deltaR    = 32*rho_s
-L_T       = R/20
+L_T       = R/5
 ky_min    = 2*math.pi/deltaR
 -- grid parameters: number of cells
 N_X = 4
