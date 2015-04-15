57b452d5f6e8595bf14b34d16ed98538704742eb
Eric Shi <eshi@pppl.gov>
2015-04-15 14:55:15 -0400

    Stuff

--- eric/test5d/etg/test0.sh
+++ eric/test5d/etg/test0.sh
@@ -1,16 +1,18 @@
 #!/bin/bash 
-#PBS -N 5d-t0
+#PBS -N par-5d
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=1:ppn=1 
+#PBS -l nodes=1:ppn=4
 #PBS -l mem=4gb 
-#PBS -l walltime=192:00:00
+#PBS -l walltime=24:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
  
-CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i test0.lua -pc_type lu" 
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i test0.lua -pc_type lu" 
 cd $PBS_O_WORKDIR 
-$CMD 
+mpiexec -np $NPROCS $CMD 
 exit
