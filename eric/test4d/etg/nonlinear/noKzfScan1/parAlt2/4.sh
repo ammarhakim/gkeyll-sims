4d870d10616b5d53333be35c949889e5cfb826eb
Eric Shi <eshi@pppl.gov>
2015-04-18 01:23:11 -0400

    Stuff

--- eric/test4d/etg/nonlinear/noKzfScan1/parAlt2/4.sh
+++ eric/test4d/etg/nonlinear/noKzfScan1/parAlt2/4.sh
@@ -3,8 +3,8 @@
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=2:ppn=16
-#PBS -l mem=10000mb
+#PBS -l nodes=4:ppn=8
+#PBS -l mem=4000mb
 #PBS -l walltime=4:00:00
 #PBS -r n
 #PBS -V 
