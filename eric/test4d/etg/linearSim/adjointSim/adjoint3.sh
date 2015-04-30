aedc1f15c1ed5bc01a7ccd5f55fbdcb0629f4d88
Eric Shi <eshi@pppl.gov>
2015-04-30 15:05:47 -0400

    Stuff

---
+++ eric/test4d/etg/linearSim/adjointSim/adjoint3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+#PBS -N adj-3
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=1:ppn=1 
+#PBS -l mem=8000mb 
+#PBS -l walltime=12:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+ 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i adjoint3.lua -pc_type lu" 
+cd $PBS_O_WORKDIR 
+$CMD 
+exit
