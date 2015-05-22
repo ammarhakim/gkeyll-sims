b948141c0ba34d98cb3978c08159493c21b09c2a
Eric Shi <eshi@pppl.gov>
2015-05-22 01:06:14 -0400

    Stuff

--- eric/test4d/etg/linearSim/adjointSim/parSims/autoconvergence2/adjoint.sh
+++ eric/test4d/etg/linearSim/adjointSim/parSims/autoconvergence2/adjoint.sh
@@ -3,9 +3,9 @@
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=2:ppn=16
+#PBS -l nodes=8:ppn=16
 #PBS -l mem=48000mb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=72:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
