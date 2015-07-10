11260ca5723705ec37aa96aaad10ce0487995417
Eric Shi <eshi@pppl.gov>
2015-07-10 00:00:00 -0400

    Stuff

--- eric/test4d/etg/linearSim/adjointSim/bruteForce/rOverLt3/innerProduct.sh
+++ eric/test4d/etg/linearSim/adjointSim/bruteForce/rOverLt3/innerProduct.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
-#PBS -N sol-rec-0-4
+#PBS -N innerproduct-3
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=1:ppn=1 
-#PBS -l mem=8000mb 
-#PBS -l walltime=48:00:00
+#PBS -l mem=10000mb
+#PBS -l walltime=24:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
  
-CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i 0_4.lua -pc_type lu" 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i innerProduct.lua -pc_type lu" 
 cd $PBS_O_WORKDIR 
 $CMD 
 exit
