61ee5f9f0d56523917ce8b62bf6d2e52b214ae48
Eric Shi <eshi@pppl.gov>
2015-07-13 03:21:00 -0400

    Stuff

---
+++ eric/sol/solRecycling/results3/7_5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+#PBS -N sol-rec-7-5
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=1:ppn=1 
+#PBS -l mem=2000mb 
+#PBS -l walltime=72:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+ 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i 7_5.lua -pc_type lu" 
+cd $PBS_O_WORKDIR 
+$CMD 
+exit
