f6b6d6fde425c9dc788427498bd6f89730d7f741
Eric Shi <eshi@pppl.gov>
2015-07-20 14:36:40 -0400

    Stuff

--- eric/test4d/etg/linearSim/adjointSim/bruteForce/portalMatlab/matlab.sh
+++ eric/test4d/etg/linearSim/adjointSim/bruteForce/portalMatlab/matlab.sh
@@ -4,13 +4,13 @@
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=2:ppn=4 
+#PBS -l nodes=1:ppn=8 
 #PBS -l mem=1000mb
 #PBS -l walltime=1:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
-CMD="matlab -nodisplay -nosplash < hello.m" 
+CMD="matlab -nodisplay -nosplash < portalProcessMatrices.m" 
 cd $PBS_O_WORKDIR 
 $CMD 
 exit
