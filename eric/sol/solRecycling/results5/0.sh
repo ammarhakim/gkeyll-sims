c08948ed7dd2563ee8eb485dd3cf9dda19b7a85e
Eric Shi <eshi@pppl.gov>
2015-07-27 16:02:19 -0400

    Stuff

--- eric/sol/solRecycling/results5/0.sh
+++ eric/sol/solRecycling/results5/0.sh
@@ -1,17 +1,17 @@
 #!/bin/bash 
 
-#PBS -N sol-rec-5
+#PBS -N sol-rec-l-0
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=1:ppn=1 
 #PBS -l mem=2000mb 
-#PBS -l walltime=72:00:00
+#PBS -l walltime=96:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
  
-CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i 5.lua -pc_type lu" 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i 0.lua -pc_type lu -r 40" 
 cd $PBS_O_WORKDIR 
 $CMD 
 exit
---
+++ eric/sol/solRecycling/results5/2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+#PBS -N sol-rec-l-2
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=1:ppn=1 
+#PBS -l mem=2000mb 
+#PBS -l walltime=96:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+ 
+CMD="/p/gke/eshi/gkeyllall/ser-opt/gkeyll/gkeyllser -i 2.lua -pc_type lu -r 40" 
+cd $PBS_O_WORKDIR 
+$CMD 
+exit
