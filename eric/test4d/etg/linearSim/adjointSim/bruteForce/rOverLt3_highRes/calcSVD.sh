2c0cb376aa6e77b1ee07279197aa5f323c5cd636
Eric Shi <eshi@pppl.gov>
2015-07-29 14:09:15 -0400

    Stuff

---
+++ eric/test4d/etg/linearSim/adjointSim/bruteForce/rOverLt3_highRes/calcSVD.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#PBS -l walltime=24:00:00,nodes=1:ppn=1,mem=16000mb
+#PBS -j oe
+#PBS -N matlab-svd
+#
+#PBS -m ae
+#PBS -M eshi@pppl.gov
+#
+
+cd $PBS_O_WORKDIR
+matlab -nosplash -nodisplay <<EOF
+% call the function
+processSVD
+EOF
+   
+echo ""
+echo "Done at " `date`
