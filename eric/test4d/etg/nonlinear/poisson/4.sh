e47ab14d3f746007963f88a7b02248540764d8c3
Eric Shi <eshi@pppl.gov>
2015-08-07 00:00:04 -0400

    Stuff

--- eric/test4d/etg/nonlinear/poisson/4.sh
+++ eric/test4d/etg/nonlinear/poisson/4.sh
@@ -5,13 +5,13 @@
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=2:ppn=16
 #PBS -l mem=96000mb
-#PBS -l walltime=96:00:00
+#PBS -l walltime=4:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4.lua -pc_type lu" 
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
--- eric/test4d/etg/nonlinear/poisson/6.sh
+++ eric/test4d/etg/nonlinear/poisson/6.sh
@@ -11,7 +11,7 @@
 #PBS -j oe 
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 6.lua -pc_type lu" 
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 6.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
