56e35e251f5fffc72ed39e7e4d838c26fdc377f5
Eric Shi <eshi@pppl.gov>
2015-08-11 14:38:22 -0400

    Stuff

--- eric/test4d/etg/nonlinear/poisson/6old.sh
+++ eric/test4d/etg/nonlinear/poisson/6old.sh
@@ -3,8 +3,8 @@
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=1:ppn=16
-#PBS -l mem=96000mb
+#PBS -l nodes=1:ppn=4
+#PBS -l mem=16000mb
 #PBS -l walltime=24:00:00
 #PBS -r n
 #PBS -V 
