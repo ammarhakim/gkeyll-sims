2dbc507f4703fc4fd6a3b96f3505fcb7ff45ee97
Eric Shi <eshi@pppl.gov>
2015-08-23 22:59:17 -0400

    Stuff

---
+++ eric/test4d/etg/nonlinear/poisson/old.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#PBS -N nonlinear-4-old
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=2:ppn=16
+#PBS -l mem=96000mb
+#PBS -l walltime=48:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+cd $PBS_O_WORKDIR 
+mpiexec -np $NPROCS $CMD 
+exit
