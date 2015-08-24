f910453c71475dceaa46ef919ce9cc8c13d99589
Eric Shi <eshi@pppl.gov>
2015-08-24 13:43:26 -0400

    Stuff

--- eric/test4d/etg/nonlinear/poisson/4lowres.lua
+++ eric/test4d/etg/nonlinear/poisson/4lowres.lua
@@ -18,9 +18,9 @@ polyOrder = 1
 cfl = 0.05
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 20e-6
+tEnd = 40e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
-nFrames = 1000
+nFrames = 2000
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 tCurr = tStart
 
