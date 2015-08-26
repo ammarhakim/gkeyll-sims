10d3c46764fd5a3bad4736dc70ba0586f51ffe4e
Eric Shi <eshi@pppl.gov>
2015-08-25 20:34:18 -0400

    Stuff

--- eric/test4d/etg/nonlinear/poisson/4lowres_again.sh
+++ eric/test4d/etg/nonlinear/poisson/4lowres_again.sh
@@ -11,7 +11,7 @@
 #PBS -j oe 
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4lowres_again.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist -r 1000"
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4lowres_again.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
