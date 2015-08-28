a694c356105f747d6c813af186b9dfe6743f5afb
Eric Shi <eshi@pppl.gov>
2015-08-28 00:09:22 -0400

    Stuff

---
+++ eric/test4d/itg/15.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#PBS -N init-4d-15
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=4:ppn=8
+#PBS -l mem=96000mb
+#PBS -l walltime=24:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 15.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+cd $PBS_O_WORKDIR 
+mpiexec -np $NPROCS $CMD 
+exit
