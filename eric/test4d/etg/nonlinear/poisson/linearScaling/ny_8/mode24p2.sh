efde2ec88834acaeb9161cae63699cad7aa00f52
Eric Shi <eshi@pppl.gov>
2015-09-11 15:14:29 -0400

    Stuff

---
+++ eric/test4d/etg/nonlinear/poisson/linearScaling/ny_8/mode24p2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+#PBS -N mode24p2
+#-- mail on execution("b"), termination ("e"), or interruption ("a")
+#PBS -m ae
+#PBS -M eshi@pppl.gov 
+#PBS -l nodes=4:ppn=4
+#PBS -l mem=16000mb
+#PBS -l walltime=48:00:00
+#PBS -r n
+#PBS -V 
+#PBS -j oe 
+NPROCS=`wc -l < $PBS_NODEFILE`
+
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i mode24p2.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+cd $PBS_O_WORKDIR 
+mpiexec -np $NPROCS $CMD 
+exit
