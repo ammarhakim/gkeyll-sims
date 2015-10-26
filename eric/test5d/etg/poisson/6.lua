90bc6176457a6acac9ddb33ab001bd540a92cefa
Eric Shi <eshi@pppl.gov>
2015-10-26 16:52:25 -0400

    Stuff

--- eric/test5d/etg/poisson/6.lua
+++ eric/test5d/etg/poisson/6.lua
@@ -17,9 +17,9 @@ polyOrder = 1
 cfl = 0.05
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 0.1e-6
+tEnd = 10e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
-nFrames = 1
+nFrames = 100
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 
 -- physical parameters
