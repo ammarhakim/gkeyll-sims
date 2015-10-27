f0510e7610c1bb0964da269e7824fa6286841c8c
Eric Shi <eshi@pppl.gov>
2015-10-27 14:40:00 -0400

    Stuff

--- eric/test4d/itg/longY/4a.lua
+++ eric/test4d/itg/longY/4a.lua
@@ -24,9 +24,9 @@ polyOrder = 1
 cfl = 0.05
 -- parameters to control time-stepping
 tStart = 0.0
-tEnd = 15e-6
+tEnd = 30e-6
 dtSuggested = 0.1*tEnd -- initial time-step to use (will be adjusted)
-nFrames = 1500
+nFrames = 3000
 tFrame = (tEnd-tStart)/nFrames -- time between frames
 tCurr = tStart
 
--- eric/test4d/itg/longY/4a.sh
+++ eric/test4d/itg/longY/4a.sh
@@ -11,7 +11,7 @@
 #PBS -j oe 
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4a.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 4a.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist -r 15"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
