ba6dfaed1e3765ea91514f6c95b37e08c576891a
Eric Shi <eshi@pppl.gov>
2015-10-30 13:34:04 -0400

    Stuff

--- eric/test5d/etg/poisson/6a.sh
+++ eric/test5d/etg/poisson/6a.sh
@@ -12,7 +12,7 @@
 #PBS -q dtest
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 6a.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist" 
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i 6a.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist -r 100" 
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
