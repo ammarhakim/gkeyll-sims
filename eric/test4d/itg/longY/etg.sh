c242b21c176e036f0c368fc8c5999e986f6a8fd8
Eric Shi <eshi@pppl.gov>
2015-11-04 17:28:30 -0500

    Stuff

--- eric/test4d/itg/longY/etg.sh
+++ eric/test4d/itg/longY/etg.sh
@@ -5,14 +5,14 @@
 #PBS -M eshi@pppl.gov 
 #PBS -l nodes=4:ppn=32
 #PBS -l mem=122000mb
-#PBS -l walltime=24:00:00
+#PBS -l walltime=48:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
 #PBS -q dtest
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i etg.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i etg.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist -r 76"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
