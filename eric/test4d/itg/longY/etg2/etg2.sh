8469b6c3f16b157c04bc368d8743ebccc5b5e13e
Eric Shi <eshi@pppl.gov>
2015-11-05 17:18:32 -0500

    Stuff

--- eric/test4d/itg/longY/etg2/etg2.sh
+++ eric/test4d/itg/longY/etg2/etg2.sh
@@ -3,13 +3,13 @@
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=8:ppn=16
+#PBS -l nodes=4:ppn=32
 #PBS -l mem=122000mb
 #PBS -l walltime=72:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
-#PBS -q dtest
+#PBS -q kruskal
 NPROCS=`wc -l < $PBS_NODEFILE`
 
 CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i etg1.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
