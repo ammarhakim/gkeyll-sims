cce6134e0c5af5ca72b1958d31d543bf35c17af2
Eric Shi <eshi@pppl.gov>
2015-11-05 17:10:51 -0500

    Stuff

--- eric/test4d/itg/longY/etg2/etg2.sh
+++ eric/test4d/itg/longY/etg2/etg2.sh
@@ -1,17 +1,18 @@
 #!/bin/bash 
-#PBS -N itg-ions-5
+#PBS -N etg-2
 #-- mail on execution("b"), termination ("e"), or interruption ("a")
 #PBS -m ae
 #PBS -M eshi@pppl.gov 
-#PBS -l nodes=4:ppn=32
+#PBS -l nodes=8:ppn=16
 #PBS -l mem=122000mb
-#PBS -l walltime=25:00:00
+#PBS -l walltime=72:00:00
 #PBS -r n
 #PBS -V 
 #PBS -j oe 
+#PBS -q dtest
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i ions5.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i etg1.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist"
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
