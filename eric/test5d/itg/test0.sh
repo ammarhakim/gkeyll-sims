f1feee06850a95086315247d02d28d3572f843a4
Eric Shi <eshi@pppl.gov>
2015-11-06 11:57:27 -0500

    Stuff

--- eric/test5d/itg/test0.sh
+++ eric/test5d/itg/test0.sh
@@ -9,10 +9,9 @@
 #PBS -r n
 #PBS -V 
 #PBS -j oe
-#PBS -q dtest
 NPROCS=`wc -l < $PBS_NODEFILE`
 
-CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i test0.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist" 
+CMD="/p/gke/eshi/gkeyllall/par-opt/gkeyll/gkeyll -i test0.lua -pc_type lu -pc_factor_mat_solver_package superlu_dist -r 117" 
 cd $PBS_O_WORKDIR 
 mpiexec -np $NPROCS $CMD 
 exit
